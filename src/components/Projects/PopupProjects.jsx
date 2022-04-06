ca0972d8d3fd11ee4e853bf536c43afd5ea2f6f3
nicholastorr <52306328+nicholastorr@users.noreply.github.com>
2022-04-06 09:28:14 -0400

    update: first project put picture and description, need to edit still

--- src/components/Projects/PopupProjects.jsx
+++ src/components/Projects/PopupProjects.jsx
@@ -2,6 +2,46 @@ import React from "react";
 import { StaticImage } from 'gatsby-plugin-image';
 import CloseIcon from '@mui/icons-material/Close';
 import './style.css';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div">
+        be{bull}nev{bull}o{bull}lent
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
  
 const Popup = props => {
 
@@ -10,9 +50,12 @@ const Popup = props => {
       <div className="box">
         <CloseIcon className="close-icon" onClick={props.handleClose} />
         <h1>HTM MBS</h1>
-        <div style= {{dislay: "flex", flexDirection: "row"}}>
-          <StaticImage src="../../images/standoffs-img-1.png"/>
-          <StaticImage src="../../images/standoffs-img-2.png" />
+        <div style= {{display: "flex", flexDirection: "row", width: "100%"}}>
+          <StaticImage src="../../images/mbs-standoffs-2.png" style={{width: "75%"}}/>
+          <Box sx={{ minWidth: 275, width: "25%" }}>
+            <Card variant="outlined">{card}</Card>
+          </Box>
+
         </div>
       </div>
     </div>
