5fcba72ece6c2e6a77f6ed709dde7ec2b43b909f
nicholastorr <52306328+nicholastorr@users.noreply.github.com>
2022-04-12 14:09:33 -0400

    update: project card, added tech stack buttons

--- src/components/Projects/PopupProjects.jsx
+++ src/components/Projects/PopupProjects.jsx
@@ -8,33 +8,40 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Collapse from '@mui/material/Collapse';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const card = (
   <React.Fragment>
     <CardContent>
       <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
+        Mbs-Standoffs
       </Typography>
       <Typography variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
+
       </Typography>
       <Typography variant="body2">
-        well meaning and kindly.
+        <h3>Large scale e-commerce site containing over 60k unique products that averages over 7k sales a month. Also a part of the HTM-MBS suite of sites that are built on top of the same Drupal CMS platform.
+        Other sites built include</h3>
+        </Typography>
+        <br /> 
+        <a>https://mbs-signsupply.com/</a> 
         <br />
-        {'"a benevolent smile"'}
-      </Typography>
+        <a>https://mbs-hanging-systems.com/</a>
+        <br />
     </CardContent>
     <CardActions>
       <Button size="small">Learn More</Button>
@@ -44,6 +51,11 @@ const card = (
 
  
 const Popup = props => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <div className="popup-box">
@@ -51,9 +63,47 @@ const Popup = props => {
         <CloseIcon className="close-icon" onClick={props.handleClose} />
         <h1>HTM MBS</h1>
         <div style= {{display: "flex", flexDirection: "row", width: "100%"}}>
-          <StaticImage src="../../images/mbs-standoffs-2.png" style={{width: "75%"}}/>
-          <Box sx={{ minWidth: 275, width: "25%" }}>
+          <StaticImage src="../../images/mbs-standoffs-2.png" style={{width: "50%"}}/>
+          <Box sx={{ minWidth: 275, width: "50%" }}>
             <Card variant="outlined">{card}</Card>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon /> <h5>Tech Stack</h5>
+            </ExpandMore>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+        <Box sx={{ 
+          '& button': { m: 1 },
+          fontSize: "50px" }}>
+      <div>
+      <Button style={{fontSize: "30px"}} variant="outlined" size="large">
+          Small
+        </Button>
+        <Button style={{fontSize: "30px"}} variant="outlined" size="large">
+          Medium
+        </Button>
+        <Button style={{fontSize: "30px"}} variant="outlined" size="large">
+          Large
+        </Button>
+      </div>
+      <div>
+      <Button style={{fontSize: "30px"}} variant="outlined" size="large">
+          Small
+        </Button>
+        <Button style={{fontSize: "30px"}} variant="outlined" size="large">
+          Medium
+        </Button>
+        <Button style={{fontSize: "30px"}} variant="outlined" size="large">
+          Large
+        </Button>
+      </div>
+    </Box>
+        </CardContent>
+        </Collapse>
           </Box>
 
         </div>
