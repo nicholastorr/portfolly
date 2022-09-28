8cd9d007dcda5b7c6e408887aab1edecc6f4c509
nicholastorr <nick.torres213@gmail.com>
2022-09-28 15:17:42 -0400

    fixed image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -40,7 +40,7 @@ const Header = () => {
               </Link>
             </span>
           </p>
-          <img src="https://imgur.com/qrmBS7M" alt="headshot" />
+          <img src="https://i.imgur.com/qrmBS7M.jpeg" alt="headshot" />
         </Fade>
       </Container>
     </section>
